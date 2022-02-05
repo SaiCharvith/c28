a3b13b33c7c2300baa6c9be64d489aa9965ca17b
SaiCharvith <88228267+SaiCharvith@users.noreply.github.com>
2022-02-05 17:20:06 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,33 @@
+class Slingshot{
+    constructor(bodyA,pointB) {
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+        }
+        this.pointB=pointB
+        this.chain=Constraint.create(options) 
+        World.add(world,this.chain)
+       
+}
+    fly(){
+this.chain.bodyA=null
+//The body is no longer attached to the chain
+}
+    display(){
+        
+        if(this.chain.bodyA){
+            var pointA=this.chain.bodyA.position;
+            var pointB=this.pointB
+            strokeWeight(4)
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+    }
+}
+    
+        
+
+    
+
+
